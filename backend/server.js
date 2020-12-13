7953a8f1a8b8640035acbd6d81996cb165abfb31
Ryan Anderson <ryanaanderson0@gmail.com>
2020-12-13 12:27:05 -0800

    changed app.get sendFile location

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ if(node_uri === 'production') {
   app.use(express.static( 'build'))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    res.sendFile( "build", "index.html");
   });
 } 
 
