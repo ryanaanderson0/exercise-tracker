6e8643a6b62393fde71989d85943fcf5772025e8
Ryan Anderson <ryanaanderson0@gmail.com>
2020-12-24 11:52:10 -0800

    POST and GET paths chagned to heroku http

--- frontend/src/components/create-athlete.component.js
+++ frontend/src/components/create-athlete.component.js
@@ -28,7 +28,7 @@ export default class CreateUser extends Component {
 
     console.log(user);
 
-    axios.post('http://localhost:5000/athletes/add', user)
+    axios.post('https://lit-bastion-77590.herokuapp.com/athletes/add', user)
       .then(res => console.log(res.data));
 
     this.setState({
